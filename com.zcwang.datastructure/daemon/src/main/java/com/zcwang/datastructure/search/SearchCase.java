1f9a7b2e1c28c5db4916099e4b99ad2a8b1ce408
superman-zcwang <18647679024@163.com>
2018-07-06 09:30:05 +0800

    修改

--- com.zcwang.datastructure/daemon/src/main/java/com/zcwang/datastructure/search/SearchCase.java
+++ com.zcwang.datastructure/daemon/src/main/java/com/zcwang/datastructure/search/SearchCase.java
@@ -1,5 +1,6 @@
 package com.zcwang.datastructure.search;
 
+
 /**
  * 功能 :
  * date : 2018/7/5
@@ -10,4 +11,24 @@ package com.zcwang.datastructure.search;
  * @since : JDK 1.8
  */
 public class SearchCase {
+
+    public static void main(String args[]) {
+        SearchCase searchCase=new SearchCase();
+        searchCase.findDuplicateEle();
+    }
+
+    /**
+     * 在给定的数组中发现两个重复出现的元素：
+     * 给定一个含有n+2个元素的数组，每个元素的取值范围是1～n，并且除了两个元素出现两次外，其他元素均出现一次，求这两个重复的元素
+     */
+    public void findDuplicateEle(){
+        int elements[]=new int[]{4,2,4,5,2,3,1};
+        for (int i = 0; i < elements.length; i++) {
+            for (int j = i+1; j < elements.length; j++) {
+                if (elements[i]==elements[j]){
+                    System.out.println(elements[i]);
+                }
+            }
+        }
+    }
 }
